2b387d6c8a6b73eedc304386256e012a10635398
Michal Vala <mvala@redhat.com>
2021-06-10 16:00:37 +0200

    read rules from yaml file (#1)
    Signed-off-by: Michal Vala <mvala@redhat.com>

--- cmd/header-rewrite-proxy/main.go
+++ cmd/header-rewrite-proxy/main.go
@@ -3,6 +3,8 @@ package main
 import (
     "flag"
     "fmt"
+    "gopkg.in/yaml.v3"
+    "io/ioutil"
     "log"
     "net/http"
     "net/http/httputil"
@@ -14,22 +16,33 @@ type ProxyHandler struct {
 }
 
 type Conf struct {
-    upstream string
-    bind string
+    upstream  *url.URL
+    bind      string
+    rulesConf string
+    rules     *Rules
 }
 
+type Rules struct {
+    Rules []Rule `yaml:"rules"`
+}
+
+type Rule struct {
+    From         string `yaml:"from"`
+    To           string `yaml:"to"`
+    Prefix       string `yaml:"prefix"`
+    KeepOriginal bool   `yaml:"keep-original"`
+}
+
+var conf *Conf
+
 func main() {
-    conf, err := parseArgs()
+    parsedConf, err := parseConfig()
     if err != nil {
         panic(err)
     }
+    conf = parsedConf
 
-    remote, err := url.Parse(conf.upstream)
-    if err != nil {
-        panic(err)
-    }
-
-    proxy := httputil.NewSingleHostReverseProxy(remote)
+    proxy := httputil.NewSingleHostReverseProxy(conf.upstream)
     http.Handle("/", &ProxyHandler{proxy})
     log.Printf("Listening on '%s' ...\n", conf.bind)
     err = http.ListenAndServe(conf.bind, nil)
@@ -38,24 +51,63 @@ func main() {
     }
 }
 func (ph *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    token := r.Header.Get("X-Forwarded-Access-Token")
-    if token != "" {
-        log.Println("Writing token from 'X-Forwarded-Access-Token' into 'Authorization' header")
-        r.Header.Set("Authorization", "Bearer " + token)
+    for _, rule := range conf.rules.Rules {
+        headerValue := r.Header.Get(rule.From)
+        if headerValue != "" {
+            if len(rule.Prefix) > 0 {
+                headerValue = rule.Prefix + headerValue
+            }
+            r.Header.Set(rule.To, headerValue)
+            if !rule.KeepOriginal {
+                r.Header.Del(rule.From)
+            }
+        }
     }
+
     ph.p.ServeHTTP(w, r)
 }
 
-func parseArgs() (*Conf, error) {
+func parseConfig() (*Conf, error) {
     conf := Conf{}
-    flag.StringVar(&conf.upstream, "upstream", "", "full url to upstream application")
-    flag.StringVar(&conf.bind, "bind", ":8080", "listen on")
+    upstream := ""
+    flag.StringVar(&upstream, "upstream", "", "Full url to upstream application.")
+    flag.StringVar(&conf.bind, "bind", ":8080", "Listen on. Default ':8080'")
+    flag.StringVar(&conf.rulesConf, "rules", "rules.yaml", "Path to rules file. Default 'rules.yaml'")
 
     flag.Parse()
 
-    if conf.upstream == "" {
+    // verify, parse and set upstream
+    if upstream == "" {
         return nil, fmt.Errorf("upstream parameter must be set")
     }
+    upstreamUrl, err := url.Parse(upstream)
+    if err != nil {
+        return nil, err
+    }
+    conf.upstream = upstreamUrl
+
+    // read rules file
+    parsedRules, err := readRulesConf(conf.rulesConf)
+    if err != nil {
+        return nil, err
+    }
+    conf.rules = parsedRules
+    log.Printf("Using rules: '%+v'", conf.rules)
 
     return &conf, nil
+}
+
+func readRulesConf(filename string) (*Rules, error) {
+    buf, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    rules := &Rules{}
+    err = yaml.Unmarshal(buf, rules)
+    if err != nil {
+        return nil, fmt.Errorf("in file %q: %v", filename, err)
+    }
+
+    return rules, nil
 }
