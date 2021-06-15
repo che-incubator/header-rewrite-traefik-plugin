302982d8ae035bb6177917b6388a326a9ea86f40
Michal Vala <mvala@redhat.com>
2021-06-15 08:19:14 +0200

    cleanup, tests, readme, licenses, ...
    Signed-off-by: Michal Vala <mvala@redhat.com>

---
+++ pkg/proxy/proxy_test.go
@@ -0,0 +1,93 @@
+// Package proxy
+// Copyright (c) 2021 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+package proxy
+
+import (
+    "net/http"
+    "net/http/httputil"
+    "testing"
+)
+
+func TestHeaderRewritten(t *testing.T) {
+    headers := &http.Header{}
+    headers.Set("hello", "world")
+
+    rewriteHeaders(headers, &Rules{Rules: []Rule{
+        {
+            From:         "hello",
+            To:           "goodbye",
+            Prefix:       "",
+            KeepOriginal: true,
+        },
+    }})
+
+    if headers.Get("goodbye") != "world" {
+        t.Errorf("header 'hello' should be rewritten to new key 'goodbye'")
+    }
+
+    if headers.Get("hello") != "world" {
+    }
+}
+
+
+func TestKeepOriginal(t *testing.T) {
+    headers := &http.Header{}
+    headers.Set("hello", "world")
+
+    rewriteHeaders(headers, &Rules{Rules: []Rule{
+        {
+            From:         "hello",
+            To:           "goodbye",
+            Prefix:       "",
+            KeepOriginal: false,
+        },
+    }})
+
+    if headers.Get("goodbye") != "world" {
+        t.Errorf("header 'hello' should be rewritten to new key 'goodbye'")
+    }
+
+    if headers.Get("hello") != "" {
+        t.Errorf("original header should be removed")
+    }
+}
+
+func TestPrefix(t *testing.T) {
+    headers := &http.Header{}
+    headers.Set("hello", "world")
+
+    rewriteHeaders(headers, &Rules{Rules: []Rule{
+        {
+            From:         "hello",
+            To:           "goodbye",
+            Prefix:       "prefix ",
+            KeepOriginal: true,
+        },
+    }})
+
+    if headers.Get("goodbye") != "prefix world" {
+        t.Errorf("header 'hello' should be rewritten to new key 'goodbye'")
+    }
+
+    if headers.Get("hello") != "world" {
+        t.Errorf("original header should not be removed or modified")
+    }
+}
+
+func prepareTestProxy(rules *Rules) *Handler {
+    return &Handler{proxy: &httputil.ReverseProxy{}, conf: &Conf{
+        Upstream:  nil,
+        Bind:      "",
+        RulesConf: "",
+        Rules:     rules,
+    }}
+}
